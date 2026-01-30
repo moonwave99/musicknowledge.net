b7f8b11f13df4099f9493bef8c361f68f81a6d5c
moonwave99 <diego.caponera@gmail.com>
2026-01-30 18:40:49 +0100

    Draft pages

---
+++ data/lessons.md
@@ -0,0 +1,5 @@
+---
+title: Private Lessons
+---
+
+WIP
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const NAV_SCROLL_THRESHOLD = 100;
 
 window.addEventListener("DOMContentLoaded", init);
 
-function init() {
+async function init() {
   const nav = document.querySelector(".nav");
   const navToggler = document.querySelector(".navToggler");
   navToggler.addEventListener("click", () =>
@@ -12,5 +12,27 @@ function init() {
   window.addEventListener("scroll", () => {
     nav.classList.toggle("scrolled", window.scrollY > NAV_SCROLL_THRESHOLD);
     document.body.classList.remove("isNavOpen");
+  });
+
+  await preloadAssets();
+}
+
+function preloadAssets() {
+  return Promise.allSettled(
+    [...document.querySelectorAll('link[rel="preload"][as="image"]')].map(
+      (node) =>
+        preload(node.href).then(() =>
+          document.querySelector(node.dataset.for)?.classList.add("loaded"),
+        ),
+    ),
+  );
+}
+
+function preload(imgSrc) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = resolve;
+    img.onerror = reject;
+    img.src = imgSrc;
   });
 }
