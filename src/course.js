7f9ccb8e105ed4942f27b921448dec91d6fbcd02
moonwave99 <diego.caponera@gmail.com>
2026-08-03 22:02:37 +0200

    Use external piano and abc deps

--- src/course.js
+++ src/course.js
@@ -1,223 +1,86 @@
-import { synth, renderAbc } from "abcjs";
-import { Paino } from "@moonwave99/paino";
-import { midiToNoteName } from "@tonaljs/midi";
+import { initAbc } from "@music-ui/abc";
+import { Piano } from "@music-ui/piano";
 import "abcjs/abcjs-audio.css";
-import "@moonwave99/paino/src/styles/paino.css";
+import "@music-ui/piano/dist/styles/index.css";
 
 window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-  document.querySelectorAll("[data-piano]").forEach((el) => initPiano(el));
+  document.querySelectorAll("[data-piano]").forEach(initPiano);
 
-  const controls = await Promise.all(
-    [...document.querySelectorAll("[data-abc]")].map(initABC),
-  );
+  await Promise.all(
+    [...document.querySelectorAll("[data-abc]")].map(async (element, index) => {
+      const content = element.querySelector("code").textContent;
+      const staffElement = document.createElement("div");
+      staffElement.classList.add("staff");
+      const audioControlsElement = document.createElement("div");
+      audioControlsElement.classList.add("audio-controls");
 
-  controls.forEach(({ cursorControl }) => {
-    cursorControl?.onPlayback((index) => {
-      controls.forEach((c) => {
-        if (index === c.cursorControl?.index) {
-          return;
-        }
-        c.synthControl?.pause();
-        c.synthControl?.restart();
-      });
-    });
-  });
-}
+      element.append(staffElement);
+      element.append(audioControlsElement);
 
-function parsePianoData(data) {
-  return data.split("\n").reduce(
-    (memo, line) => {
-      const match = line.match(/\w+:/);
-      if (match) {
-        const [key, notes] = line.split(":");
-        return {
-          ...memo,
-          [key]: notes.trim().split(" "),
-        };
-      } else {
-        return {
-          ...memo,
-          notes: line.trim().split(" "),
-        };
+      const hidePlayer = Boolean(element.dataset.hidePlayer);
+      const hidePiano = Boolean(element.dataset.hidePiano);
+
+      let piano;
+
+      if (!hidePiano) {
+        const pianoElement = document.createElement("div");
+        pianoElement.dataset.piano = true;
+        element.append(pianoElement);
+        piano = initPiano(pianoElement);
       }
-    },
-    { notes: [] },
+
+      await initAbc({
+        id: element.dataset.id || String(index + 1),
+        content,
+        staffElement,
+        audioControlsElement,
+        hidePlayer,
+        onNotesChange: (notes) => {
+          if (!piano) {
+            return;
+          }
+          piano.setNotes(notes);
+        },
+      });
+    }),
   );
 }
 
 function initPiano(el) {
-  const data = el.querySelector("code").textContent.trim();
-  if (!data) {
-    return;
-  }
-
-  const piano = document.createElement("div");
-  const pianoWrapper = document.createElement("div");
-  pianoWrapper.appendChild(piano);
-  pianoWrapper.classList.add("pianoWrapper");
-  el.appendChild(pianoWrapper);
-
-  const pianoController = new Paino({
-    el: piano,
+  return new Piano({
+    el,
     octaves: 5,
     startOctave: 2,
-  });
-  pianoController.render();
-  pianoController.setNotes(parsePianoData(data));
+  })
+    .render()
+    .setNotes(parsePianoData(el.querySelector("code")?.textContent.trim()));
 }
 
-async function initABC(el, index) {
-  const data = el.querySelector("code").textContent.trim();
-  if (!data) {
-    return;
-  }
-
-  const staff = document.createElement("div");
-  const audio = document.createElement("div");
-  const pianoWrapper = document.createElement("div");
-  const piano = document.createElement("div");
-
-  el.appendChild(staff);
-  el.appendChild(audio);
-  el.appendChild(pianoWrapper);
-  pianoWrapper.appendChild(piano);
-  pianoWrapper.classList.add("pianoWrapper");
-
-  const visualObj = renderAbc(staff, data, {
-    responsive: "resize",
-    add_classes: true,
-  }).at(0);
-
-  const hideMeter = visualObj.getMeter()?.value[0].den === "1";
-  const hidePiano = typeof el.dataset.hidepiano !== "undefined";
-  const hidePlayer = typeof el.dataset.hideplayer !== "undefined";
-
-  if (hideMeter) {
-    staff.querySelector(".abcjs-time-signature").style.display = "none";
-  }
-
-  let pianoController;
-  if (!hidePiano) {
-    pianoController = new Paino({
-      el: piano,
-      octaves: 5,
-      startOctave: 2,
-    });
-    pianoController.render();
-  } else {
-    piano.remove();
-  }
-
-  if (hidePlayer) {
-    audio.remove();
-    return {};
-  }
-
-  if (!synth.supportsAudio()) {
-    audio.innerHTML =
-      "<div class='audio-error'>Audio is not supported in this browser.</div>";
-    return;
-  }
-
-  const cursorControl = new CursorControl({
-    index,
-    el: staff,
-    onNotesChange: (notes) => {
-      pianoController?.setNotes(
-        notes
-          .filter(({ cmd }) => cmd === "note")
-          .map(({ pitch }) => midiToNoteName(pitch)),
-      );
-    },
-  });
-  const synthControl = new synth.SynthController();
-  synthControl.load(audio, cursorControl, {
-    displayLoop: true,
-    displayRestart: true,
-    displayPlay: true,
-    displayProgress: true,
-    displayWarp: true,
-  });
-  const midiBuffer = new synth.CreateSynth();
-  await midiBuffer.init({ visualObj });
-  synthControl.setTune(visualObj, true);
-
-  return { synthControl, cursorControl };
-}
-
-class CursorControl {
-  constructor({ index, el, beatSubdivisions = 2, onNotesChange }) {
-    this.index = index;
-    this.el = el;
-    this.beatSubdivisions = beatSubdivisions;
-    this.onNotesChange = onNotesChange;
-  }
-  onPlayback(callback) {
-    this._onPlayback = callback;
-  }
-  onStart() {
-    if (!this.el.querySelector("svg .abcjs-cursor")) {
-      this._createCursor();
-    }
-    if (!this._onPlayback) {
-      return;
-    }
-    this._onPlayback(this.index);
-  }
-  onEvent(event) {
-    if (event.measureStart && event.left === null) {
-      return;
-    }
-
-    if (this.onNotesChange) {
-      this.onNotesChange(event.midiPitches);
-    }
-
-    this.el
-      .querySelectorAll("svg .highlight")
-      .forEach((x) => x.classList.remove("highlight"));
-
-    event.elements.forEach((x) =>
-      x.forEach((y) => y.classList.add("highlight")),
-    );
-
-    const cursor = this.el.querySelector("svg .abcjs-cursor");
-    if (!cursor) {
-      return;
-    }
-    cursor.setAttribute("x1", event.left - 2);
-    cursor.setAttribute("x2", event.left - 2);
-    cursor.setAttribute("y1", event.top);
-    cursor.setAttribute("y2", event.top + event.height);
-  }
-  onFinished() {
-    if (this.onNotesChange) {
-      this.onNotesChange([]);
-    }
-    this.el
-      .querySelectorAll("svg .highlight")
-      .forEach((x) => x.classList.remove("highlight"));
-    const cursor = this.el.querySelector("svg .abcjs-cursor");
-    if (!cursor) {
-      return;
-    }
-    cursor.setAttribute("x1", 0);
-    cursor.setAttribute("x2", 0);
-    cursor.setAttribute("y1", 0);
-    cursor.setAttribute("y2", 0);
-  }
-  _createCursor() {
-    const cursor = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "line",
-    );
-    cursor.setAttribute("class", "abcjs-cursor");
-    cursor.setAttributeNS(null, "x1", 0);
-    cursor.setAttributeNS(null, "y1", 0);
-    cursor.setAttributeNS(null, "x2", 0);
-    cursor.setAttributeNS(null, "y2", 0);
-    this.el.querySelector("svg").appendChild(cursor);
-  }
+function parsePianoData(data) {
+  return !data
+    ? []
+    : Object.values(
+        data.split("\n").reduce(
+          (memo, line) => {
+            const match = line.match(/\w+:/);
+            if (match) {
+              const [key, notes] = line.split(":");
+              return {
+                ...memo,
+                [key]: notes.trim().split(" "),
+              };
+            }
+            return {
+              ...memo,
+              notes: line.trim().split(" "),
+            };
+          },
+          { _notes: [] },
+        ),
+      )
+        .filter((x) => x.length)
+        .map((x) => x.join(" "))
+        .join(", ");
 }
