5145ae9452b621c1919ced349aa1b53cd687db48
moonwave99 <diego.caponera@gmail.com>
2026-08-08 14:26:57 +0200

    Update deps

--- src/course.js
+++ src/course.js
@@ -1,51 +1,106 @@
 import { initAbc } from "@music-ui/abc";
 import { Piano } from "@music-ui/piano";
+import { Player, getAbcScore } from "@music-ui/core";
 import "abcjs/abcjs-audio.css";
 import "@music-ui/piano/dist/styles/index.css";
 
 window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
+  const player = new Player();
+
   document.querySelectorAll("[data-piano]").forEach(initPiano);
 
-  await Promise.all(
-    [...document.querySelectorAll("[data-abc]")].map(async (element, index) => {
-      const content = element.querySelector("code").textContent;
-      const staffElement = document.createElement("div");
-      staffElement.classList.add("staff");
-      const audioControlsElement = document.createElement("div");
-      audioControlsElement.classList.add("audio-controls");
+  document.querySelectorAll("[data-abc]").forEach((element, index) => {
+    const { id, content, hidePiano, hidePlayer, bpm } = parseElementOptions(
+      element,
+      index,
+    );
 
-      element.append(staffElement);
-      element.append(audioControlsElement);
+    const staffElement = document.createElement("div");
+    staffElement.classList.add("staff");
+    element.append(staffElement);
 
-      const hidePlayer = Boolean(element.dataset.hidePlayer);
-      const hidePiano = Boolean(element.dataset.hidePiano);
+    initAbc({ id, content, staffElement });
 
-      let piano;
+    const score = getAbcScore({ id, bpm, content });
 
-      if (!hidePiano) {
-        const pianoElement = document.createElement("div");
-        pianoElement.dataset.piano = true;
-        element.append(pianoElement);
-        piano = initPiano(pianoElement);
-      }
-
-      await initAbc({
-        id: element.dataset.id || String(index + 1),
-        content,
-        staffElement,
-        audioControlsElement,
-        hidePlayer,
-        onNotesChange: (notes) => {
-          if (!piano) {
-            return;
-          }
-          piano.setNotes(notes);
+    if (!hidePlayer) {
+      const { play, pause, stop } = createControls(element, {
+        play: () => {
+          player.setScore(score);
+          player.play();
+          play.disabled = true;
+          pause.disabled = false;
+          stop.disabled = false;
+        },
+        pause: () => {
+          player.pause();
+          play.disabled = false;
+          pause.disabled = true;
+          stop.disabled = false;
+        },
+        stop: () => {
+          player.stop();
+          play.disabled = false;
+          pause.disabled = true;
+          stop.disabled = true;
         },
       });
-    }),
-  );
+    }
+
+    if (hidePiano) {
+      return;
+    }
+
+    const pianoElement = document.createElement("div");
+    pianoElement.dataset.piano = "";
+    element.append(pianoElement);
+    const piano = initPiano(pianoElement);
+
+    player.on("playing", ({ playedNotes, activeId }) => {
+      if (activeId !== score.id) {
+        piano.setNotes([]);
+        return;
+      }
+      piano.setNotes(playedNotes);
+    });
+
+    player.on("stop", () => {
+      piano.setNotes([]);
+    });
+  });
+}
+
+function parseElementOptions(element, index) {
+  return {
+    id: element.dataset.id || String(index + 1),
+    hidePlayer: Boolean(element.dataset.hidePlayer),
+    hidePiano: Boolean(element.dataset.hidePiano),
+    bpm: Number(element.dataset.bpm) || 120,
+    content: element.querySelector("code").textContent,
+  };
+}
+
+function createControls(element, handlers = {}) {
+  const controls = document.createElement("div");
+  controls.classList.add("controls");
+
+  const buttons = {};
+
+  Object.entries(handlers).forEach(([name, handler]) => {
+    const button = document.createElement("button");
+    button.classList.add("control-button", `${name}-button`);
+    button.addEventListener("click", handler);
+    button.textContent = name;
+    controls.append(button);
+    button.disabled = name !== "play";
+    buttons[name] = button;
+  });
+
+  element.append(controls);
+
+  return buttons;
 }
 
 function initPiano(el) {
